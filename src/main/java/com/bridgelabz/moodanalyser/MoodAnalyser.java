deb125a0df5f1219a55c5610dec41dc827b4bb69
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-10-13 14:23:01 +0530

    Added:I am in Happy Mood message in Constructor Should Return Happy

--- src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -1,33 +1,33 @@
 package com.bridgelabz.moodanalyser;
 /*
- * "I am in sad Mood" message in Constructor Should Return SAD
+ * "I am in Happy Mood" message in Constructor Should Return Happy
  */
 public class MoodAnalyser {
 	public static String message;
 
-    // Constructor
-    public MoodAnalyser(String message) {
-        this.message = message;
-        analyseMood();
-    }
+	// Constructor
+	public MoodAnalyser(String message) {
+		this.message = message;
+		analyseMood();
+	}
 
-    //Getter
-    public String getMessage() {
-        return message;
-    }
+	//Getter
+	public String getMessage() {
+		return message;
+	}
 
-    //Setter
-    public void setMessage(String message) {
-        this.message = message;
-    }
+	//Setter
+	public void setMessage(String message) {
+		this.message = message;
+	}
 
-    public String analyseMood() {
+	public String analyseMood() {
 
-        if (message.toLowerCase().contains("sad")) {
-            return "Sad";
-        } else if (message.toLowerCase().contains("happy")) {
-        }
-        return "Happy";
-    }
+		if (message.toLowerCase().contains("sad")) {
+			return "Sad";
+		} else if (message.toLowerCase().contains("happy")) {
+		}
+		return "Happy";
+	}
 }
 
--- src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -9,5 +9,11 @@ public class MoodAnalyserTest {
 	        String actualResult = moodanalyser.analyseMood();
 	        Assert.assertEquals("Sad", actualResult);
 	    }
+	 @Test
+	    public void givenMessage_WhenProper_ShouldReturnHappy() {
+	        MoodAnalyser moodanalyser = new MoodAnalyser("I am in happy Mood");
+	        String actualResult = moodanalyser.analyseMood();
+	        Assert.assertEquals("Happy", actualResult);
+	    }
 }
 
