ed955c5a5ac272bb4242268c53d5c8cfd779457f
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-10-13 14:08:00 +0530

    Added:Ability to analyse and respond happy or sad

--- src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -2,28 +2,14 @@ package com.bridgelabz.moodanalyser;
 import org.junit.Test;
 import org.junit.Assert;
 
-public class MoodAnalyserTest
-{
-    // Test case to analyze sad mood of the user
+public class MoodAnalyserTest {
+
+    /**
+     * Rigorous Test :-)
+     */
     @Test
-    public void givenMessage_WhenProper_ShouldReturnSad() {
-        MoodAnalyser moodanalyser=new MoodAnalyser("I am in Sad Mood");
-        String actualResult=moodanalyser.analyseMood();
-        Assert.assertEquals("Sad",actualResult);
-    }
-    // Test case to analyze happy mood of the user
-    @Test
-    public void givenMessage_ContainsAny_init_ShouldReturnHappy() {
-    	MoodAnalyser moodanalyser = new MoodAnalyser("Iam in happy mood");
-    	String actualResult = moodanalyser.analyseMood();
-    	Assert.assertEquals("Happy", actualResult);
-    }
-    //Test case to Null Mood Should Return Happy
-    @Test
-    public void givenMessage_Null_ShouldReturnHappy() {
-        MoodAnalyser moodanalyser = new MoodAnalyser(null);
-        String actualResult = moodanalyser.analyseMood();
-        Assert.assertEquals("Happy", actualResult);
+    public void shouldAnswerWithTrue() {
+        Assert.assertTrue(true);
     }
 }
 
